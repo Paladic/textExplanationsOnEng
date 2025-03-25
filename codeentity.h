5dcba4371ff3a4444f79a5ca75492bc3eb32ecc3
Paladic <nak78nik@gmail.com>
2025-03-25 23:55:14 +0300

    Добавление структуры хранящей данные об переменной

--- codeentity.h
+++ codeentity.h
@@ -1,3 +1,14 @@
 #ifndef CODEENTITY_H
 #define CODEENTITY_H
+#include <QString>
+
+
+// Переменная
+struct Variable {
+
+    QString name;           // имя
+    QString type;           // тип
+    QString description;    // описание
+
+};
 #endif // CODEENTITY_H
