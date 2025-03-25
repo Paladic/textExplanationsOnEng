a774b7911e8d7d91850544fbd5b13a322913a5e7
Paladic <nak78nik@gmail.com>
2025-03-25 21:55:58 +0300

    Добавление конструкторов для класса исключения

--- teexception.cpp
+++ teexception.cpp
@@ -1,3 +1,21 @@
 #include "teexception.h"
 
-TEException::TEException() {}
+TEException::TEException(const ErrorType errorType, const QString &filename, const int line, const QList<QString> args) {
+
+    this->errorType = errorType;
+    this->filename = filename;
+    this->line = line;
+    this->args = args;
+
+}
+
+TEException::TEException(const ErrorType errorType, const QString &filename, const QList<QString> args) {
+
+    TEException(errorType, filename, 0, args);
+
+}
+
+TEException::TEException(const ErrorType errorType, const QList<QString> args) {
+
+    TEException(errorType, "", 0, args);
+}
--- teexception.h
+++ teexception.h
@@ -52,7 +52,10 @@ enum class ErrorType {
 class TEException
 {
 public:
-    TEException();
+
+    TEException(const ErrorType errorType, const QString &filename, const int line, const QList<QString> args = QList<QString>());
+    TEException(const ErrorType errorType, const QString &filename, const QList<QString> args = QList<QString>());
+    TEException(const ErrorType errorType, const QList<QString> args = QList<QString>());
 private:
 
     ErrorType errorType; // Тип ошибки
