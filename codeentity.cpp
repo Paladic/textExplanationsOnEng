c11dd20a9cad87f97540d9b36400b026d785f38e
Paladic <nak78nik@gmail.com>
2025-04-03 22:34:04 +0300

    Реализация методов привода структур к QString

--- codeentity.cpp
+++ codeentity.cpp
@@ -2,11 +2,55 @@
 
 Variable::Variable(const QString &name, const QString &type, const QString &description)
     : name(name), type(type), description(description) {}
+
+QString Variable::toQString(const QString& startLine) const {
+
+    //QString result = startLine + "Variable:";
+    //result += "\n" + startLine + "  name: " + name;
+    //result += "\n" + startLine + "  type: " + type;
+    //result += "\n" + startLine + "  description: " + description;
+
+    QString result = startLine + "Variable:";
+    result += "  name: " +          this->name;
+    result += ";  type: " +         this->type;
+    result += ";  description: " +  this->description;
+
+    return result;
+}
+
+
 Function::Function(const QString &name, const QString &type, int paramsCount, const QString &description)
     : name(name), type(type), paramsCount(paramsCount), description(description) {}
 
+QString Function::toQString(const QString& startLine) const {
+
+    QString result = startLine + "Function:";
+    result += "  name: " +          this->name;
+    result += ";  type: " +         this->type;
+    result += ";  description: " +  this->description;
+
+    return result;
+
+}
+
 Union::Union(const QString &name, const QList<Variable> &variables, const QList<Function> &functions)
     : name(name), variables(variables), functions(functions) {}
+
+QString Union::toQString(const QString& startLine) const {
+
+    QString result = startLine + "Union:";
+    result += " name: " + this->name;
+
+    QListIterator itV(this->variables);
+    while(itV.hasNext())
+        result += "\n" + itV.next().toQString(startLine + "    ");
+
+    QListIterator itF(this->functions);
+    while(itF.hasNext())
+        result += "\n" + itF.next().toQString(startLine + "    ");
+
+    return result;
+}
 
 
 
@@ -14,9 +58,57 @@ Union::Union(const QString &name, const QList<Variable> &variables, const QList<
 Structure::Structure(const QString &name, const QList<Variable> &variables, const QList<Function> &functions)
     : name(name), variables(variables), functions(functions) {}
 
+QString Structure::toQString(const QString& startLine) const {
+
+    QString result = startLine + "Structure:";
+    result += " name: " + this->name;
+
+    QListIterator itV(this->variables);
+    while(itV.hasNext())
+        result += "\n" + itV.next().toQString(startLine + "    ");
+
+    QListIterator itF(this->functions);
+    while(itF.hasNext())
+        result += "\n" + itF.next().toQString(startLine + "    ");
+
+    return result;
+}
+
+
 Class::Class(const QString &name, const QList<Variable> &variables, const QList<Function> &functions)
     : name(name), variables(variables), functions(functions) {}
+
+QString Class::toQString(const QString& startLine) const {
+
+    QString result = startLine + "Class:";
+    result += " name: " + this->name;
+
+    QListIterator itV(this->variables);
+    while(itV.hasNext())
+        result += "\n" + itV.next().toQString(startLine + "    ");
+
+    QListIterator itF(this->functions);
+    while(itF.hasNext())
+        result += "\n" + itF.next().toQString(startLine + "    ");
+
+    return result;
+
+}
 
 
 Enum::Enum(const QString &name, const QMap<QString, QString> &values)
     : name(name), values(values) {}
+
+QString Enum::toQString(const QString& startLine) const {
+
+    QString result = startLine + "Enum:";
+    result += " name: " + name;
+
+    QMapIterator itV(this->values);
+    while(itV.hasNext()) {
+        auto v = itV.next();
+        result += "\n   " + startLine + v.key() + ": " + v.value();
+    }
+    return result;
+
+}
