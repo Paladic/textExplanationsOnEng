de6ae844932a122b49d8ba3848e8e316ba23745b
Paladic <nak78nik@gmail.com>
2025-04-07 00:33:17 +0300

    Исправление конструкторов

--- teexception.cpp
+++ teexception.cpp
@@ -1,27 +1,23 @@
 #include "teexception.h"
 
-TEException::TEException(const ErrorType errorType, const QString &filename, const int line, const QList<QString> args) {
-
-    this->errorType = errorType;
-    this->filename = filename;
-    this->line = line;
-    this->args = args;
-
+TEException::TEException(const ErrorType errorType, const QString &filename, const int line, const QList<QString> args)
+    : errorType(errorType), filename(filename), line(line), args(args)
+{
 }
 
-TEException::TEException(const ErrorType errorType, const QString &filename, const QList<QString> args) {
-
-    TEException(errorType, filename, 0, args);
-
+TEException::TEException(const ErrorType errorType,  const QString &filename, const QList<QString> args)
+    : TEException(errorType, filename, 0, args)
+{
 }
 
-TEException::TEException(const ErrorType errorType, const QList<QString> args) {
 TEException::TEException(const ErrorType errorType,  const int line, const QList<QString> args)
     : TEException(errorType, "", line, args)
 {
 }
 
-    TEException(errorType, "", 0, args);
+TEException::TEException(const ErrorType errorType, const QList<QString> args)
+    : TEException(errorType, "", 0, args)
+{
 }
 
 QString TEException::what() const {
