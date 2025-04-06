1e74400c958216df90de9754124c987b1f4b7ca5
Paladic <nak78nik@gmail.com>
2025-04-07 00:59:43 +0300

    Добавление статических констант
    Константы хранят в себе данные об максимальном разрешенном диапазоне длинны или количества объектов.

--- expressionxmlparser.h
+++ expressionxmlparser.h
@@ -8,6 +8,17 @@ public:
 
     static void readDataFromXML(const QString& inputFilePath, Expression& expression);
 
+    //////////////////////////////////////////////////
+    /// Константы
+    /////////////////////////////////////////////////
+
+    static constexpr int nameMaxLength = 32;
+    static constexpr int descMaxLength = 256;
+    static constexpr int expressionMaxLength = 1024;
+    static constexpr int childElementsMaxCount = 20;
+    static constexpr int functionParamsMaxCount = 5;
+
+
 };
 
 #endif // EXPRESSIONXMLPARSER_H
