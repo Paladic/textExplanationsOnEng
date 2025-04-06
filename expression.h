29135f3527244919ef2cfa5d4e0325e7b08ec396
Paladic <nak78nik@gmail.com>
2025-04-07 00:38:26 +0300

    Добавление основных полей с которыми будет работать класс

--- expression.h
+++ expression.h
@@ -1,7 +1,23 @@
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
+
+#include "codeentity.h"
+
+#include <QHash>
+#include <QString>
+
+
 class Expression
 {
+
+    QString expression; // Текстовое выражение
+    QHash<QString, Variable> variables; //  Переменные
+    QHash<QString, Function> functions; // функции
+    // Пользовательские типы
+    QHash<QString, Union> unions; // Объединения
+    QHash<QString, Structure> structures; // Структуры
+    QHash<QString, Class> classes; // Классы
+    QHash<QString, Enum> enums; // Перечисления
 };
 
 #endif // EXPRESSION_H
