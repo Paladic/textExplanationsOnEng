81b1c54d515e401cf7181373507f9d240ade3aef
Paladic <nak78nik@gmail.com>
2025-04-07 00:39:37 +0300

    Добавление публичных гетеров и сетеров для полей

--- expression.cpp
+++ expression.cpp
@@ -1,1 +1,70 @@
 #include "expression.h"
+void Expression::setExpression(const QString &newExpression)
+{
+    expression = newExpression;
+}
+
+QString Expression::getExpression() const
+{
+    return expression;
+}
+
+QHash<QString, Variable> Expression::getVariables() const
+{
+    return variables;
+}
+
+void Expression::setVariables(const QHash<QString, Variable> &newVariables)
+{
+    variables = newVariables;
+}
+
+QHash<QString, Function> Expression::getFunctions() const
+{
+    return functions;
+}
+
+void Expression::setFunctions(const QHash<QString, Function> &newFunctions)
+{
+    functions = newFunctions;
+}
+
+QHash<QString, Union> Expression::getUnions() const
+{
+    return unions;
+}
+
+void Expression::setUnions(const QHash<QString, Union> &newUnions)
+{
+    unions = newUnions;
+}
+
+QHash<QString, Structure> Expression::getStructures() const
+{
+    return structures;
+}
+
+void Expression::setStructures(const QHash<QString, Structure> &newStructures)
+{
+    structures = newStructures;
+}
+
+QHash<QString, Class> Expression::getClasses() const
+{
+    return classes;
+}
+
+void Expression::setClasses(const QHash<QString, Class> &newClasses)
+{
+    classes = newClasses;
+}
+
+QHash<QString, Enum> Expression::getEnums() const
+{
+    return enums;
+}
+
+void Expression::setEnums(const QHash<QString, Enum> &newEnums)
+{
+    enums = newEnums;
+}
--- expression.h
+++ expression.h
@@ -18,6 +18,29 @@ class Expression
     QHash<QString, Structure> structures; // Структуры
     QHash<QString, Class> classes; // Классы
     QHash<QString, Enum> enums; // Перечисления
+
+public:
+
+    void setExpression(const QString &newExpression);
+    QString getExpression() const;
+
+    QHash<QString, Variable> getVariables() const;
+    void setVariables(const QHash<QString, Variable> &newVariables);
+
+    QHash<QString, Function> getFunctions() const;
+    void setFunctions(const QHash<QString, Function> &newFunctions);
+
+    QHash<QString, Union> getUnions() const;
+    void setUnions(const QHash<QString, Union> &newUnions);
+
+    QHash<QString, Structure> getStructures() const;
+    void setStructures(const QHash<QString, Structure> &newStructures);
+
+    QHash<QString, Class> getClasses() const;
+    void setClasses(const QHash<QString, Class> &newClasses);
+
+    QHash<QString, Enum> getEnums() const;
+    void setEnums(const QHash<QString, Enum> &newEnums);
 };
 
 #endif // EXPRESSION_H
