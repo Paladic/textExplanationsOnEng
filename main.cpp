c755fdedd33242aebdd1a17ba6612452e56f8efb
Paladic <nak78nik@gmail.com>
2025-04-07 00:46:02 +0300

    добавление функции для вывода сообщения-помощника

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+void printHelpMessage(QTextStream& cout, const QString& filename);
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,18 @@ int main(int argc, char *argv[])
     fileName = fileInfo.fileName();
 
     qDebug() << "hello world";
+}
 
     return a.exec();
+void printHelpMessage(QTextStream& cout, const QString& filename)
+{
+    cout << ".\\" + filename + " [-help | -test] [input-file] [output-file]\n";
+    cout << "-help      - Выводит сообщение-помощник. При вводе этой команды путь к файлам указывать не нужно.\n";
+    cout << "-test      - Запускает тесты. При вводе этой команды путь к файлам указывать не нужно.\n";
+    cout << "input-file - путь к входному файлу. В случае, если в пути файла присутствуют пробелы, необходимо указать путь в кавычках. Например:\n";
+    cout << "               \"C:\\\\input files\\input.txt\"\n";
+    cout << "output-file - путь к выходному файлу. Если файла не существует - он будет создан. В случае, если в пути файла присутствуют пробелы, необходимо указать путь в кавычках. Например:\n";
+    cout << "               \"C:\\\\output files\\output.txt\"\n";
+    cout << "Пример запуска: \n";
+    cout << "   .\\" + filename + " input.txt \"C:\\\\files\\New folder\\output.txt\"\n";
 }
