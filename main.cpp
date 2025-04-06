127226b9282a64ecf8233b495a23edec181e8469
Paladic <nak78nik@gmail.com>
2025-04-07 00:45:12 +0300

    получение имени запускаемого .exe файла

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QString fileName = QCoreApplication::applicationFilePath();
+    QFileInfo fileInfo(fileName);
+    fileName = fileInfo.fileName();
 
     qDebug() << "hello world";
 
