1abc83b1fe1f0a01ab76cc68f731472b5f35e8a4
Paladic <nak78nik@gmail.com>
2025-04-16 21:12:20 +0300

    Реализация метода определения латинской буквы

--- expressionxmlparser.cpp
+++ expressionxmlparser.cpp
@@ -3,4 +3,9 @@
 const QList<QString> ExpressionXmlParser::supportedDataTypesForVar = { "int", "float", "double", "char", "bool", "string" };
 
 void ExpressionXmlParser::readDataFromXML(const QString& inputFilePath, Expression &expression) {
+bool ExpressionXmlParser::isLatinLetter(const QChar c) {
+    // Явная проверка латинских букв
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
 }
