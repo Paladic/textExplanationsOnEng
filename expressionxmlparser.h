807a629d0edd72a25ea5e2a50db93105eb45bcd1
Paladic <nak78nik@gmail.com>
2025-04-16 16:50:46 +0300

    Добавление методов для работы с XML

--- expressionxmlparser.h
+++ expressionxmlparser.h
@@ -15,6 +15,15 @@ public:
     static QDomDocument readXML(const QString& filePath);
     static bool checkFileReadAccess(const QString& filePath);
     static QTemporaryFile* createTempCopy(const QString &sourceFilePath);
+
+    //////////////////////////////////////////////////
+    /// Методы для исправления XML формата
+    /////////////////////////////////////////////////
+
+    static QString escapeXmlText(const QString& text);
+    static QString fixXmlFlags(const QString& xmlString);
+    static QString fixXmlExpression(const QString& xmlString);
+    static QString fixXmlDescriptions(const QString& xmlString);
     //////////////////////////////////////////////////
     /// Константы
     /////////////////////////////////////////////////
