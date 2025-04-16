d97b0c33a2138146bc312a435461f3e21a491257
Paladic <nak78nik@gmail.com>
2025-04-16 21:13:02 +0300

    Реализация метода валидации атрибутов

--- expressionxmlparser.cpp
+++ expressionxmlparser.cpp
@@ -1,4 +1,5 @@
 #include "expressionxmlparser.h"
+#include "teexception.h"
 
 const QList<QString> ExpressionXmlParser::supportedDataTypesForVar = { "int", "float", "double", "char", "bool", "string" };
 
@@ -8,4 +9,16 @@ bool ExpressionXmlParser::isLatinLetter(const QChar c) {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
+void ExpressionXmlParser::validateAttributes(const QDomElement& curElement, const QList<QString>& attributes) {
+
+    QDomNamedNodeMap getAttributes = curElement.attributes();
+    for (int i = 0; i < getAttributes.length(); i++) {
+        QDomAttr attribute = getAttributes.item(i).toAttr();
+
+        // Проверяем, есть ли этот атрибут в списке elements
+        if (!attributes.contains(attribute.name()) || attributes.count() == 0) {
+            throw TEException(ErrorType::UnexpectedAttribute, attribute.lineNumber(), QList<QString>{attribute.name(), attributes.join("; ")});
+        }
+    }
+}
 }
