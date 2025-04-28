39c8720b9118ca88f5be44389951c2afee7d80d4
Paladic <nak78nik@gmail.com>
2025-04-28 17:43:05 +0300

    Добавление функций получения данных по названию

--- expression.cpp
+++ expression.cpp
@@ -27,6 +27,11 @@ void Expression::setVariables(const QHash<QString, Variable> &newVariables)
     variables = newVariables;
 }
 
+const Variable Expression::getVarByName(const QString &name) const
+{
+    return getVariables()->value(name);
+}
+
 const QHash<QString, Function>* Expression::getFunctions() const
 {
     return &functions;
@@ -35,6 +40,11 @@ const QHash<QString, Function>* Expression::getFunctions() const
 void Expression::setFunctions(const QHash<QString, Function> &newFunctions)
 {
     functions = newFunctions;
+}
+
+const Function Expression::getFuncByName(const QString &name) const
+{
+    return getFunctions()->value(name);
 }
 
 const QHash<QString, Union>* Expression::getUnions() const
@@ -47,8 +57,11 @@ void Expression::setUnions(const QHash<QString, Union> &newUnions)
     unions = newUnions;
 }
 
+const Union Expression::getUnionByName(const QString &name) const
 {
-    return structures;
+    return getUnions()->value(name);
+}
+
 const QHash<QString, Structure>* Expression::getStructures() const
 {
     return &structures;
@@ -57,6 +70,11 @@ const QHash<QString, Structure>* Expression::getStructures() const
 void Expression::setStructures(const QHash<QString, Structure> &newStructures)
 {
     structures = newStructures;
+}
+
+const Structure Expression::getStructByName(const QString &name) const
+{
+    return getStructures()->value(name);
 }
 
 const QHash<QString, Class>* Expression::getClasses() const
@@ -69,8 +87,11 @@ void Expression::setClasses(const QHash<QString, Class> &newClasses)
     classes = newClasses;
 }
 
+const Class Expression::getClassByName(const QString &name) const
 {
-    return enums;
+    return getClasses()->value(name);
+}
+
 const QHash<QString, Enum>* Expression::getEnums() const
 {
     return &enums;
@@ -80,6 +101,41 @@ void Expression::setEnums(const QHash<QString, Enum> &newEnums)
 {
     enums = newEnums;
 }
+
+const Enum Expression::getEnumByName(const QString &name) const
+{
+    return getEnums()->value(name);
+}
+
+const Variable Expression::getVariableByNameFromCustomData(QString varName, QString dataName) const
+{
+    // Получить пользовательский тип данных по его имени
+    CustomTypeWithFields customType = getCustomTypeByName(dataName);
+    return customType.variables.value(varName);
+}
+
+const Function Expression::getFunctionByNameFromCustomData(QString funcName, QString dataName) const
+{
+    // Получить пользовательский тип данных по его имени
+    CustomTypeWithFields customType = getCustomTypeByName(dataName);
+    return customType.functions.value(funcName);
+}
+
+const CustomTypeWithFields Expression::getCustomTypeByName(const QString &typeName) const
+{
+    CustomTypeWithFields type;
+    if (classes.contains(typeName)) {
+        type = classes.value(typeName);
+    }
+    else if (structures.contains(typeName)) {
+        type = structures.value(typeName);
+    }
+    else if (unions.contains(typeName)) {
+        type = unions.value(typeName);
+    }
+    return type;
+}
+
 QString Expression::ToQstring()
 {
     QString result = "Expression:   "  + this->expression;
--- expression.h
+++ expression.h
@@ -17,21 +17,32 @@ public:
 
     const QHash<QString, Variable>* getVariables() const;
     void setVariables(const QHash<QString, Variable> &newVariables);
+    const Variable getVarByName(const QString &name) const;
 
     const QHash<QString, Function>* getFunctions() const;
     void setFunctions(const QHash<QString, Function> &newFunctions);
+    const Function getFuncByName(const QString &name) const;
 
     const QHash<QString, Union>* getUnions() const;
     void setUnions(const QHash<QString, Union> &newUnions);
+    const Union getUnionByName(const QString &name) const;
 
     const QHash<QString, Structure>* getStructures() const;
     void setStructures(const QHash<QString, Structure> &newStructures);
+    const Structure getStructByName(const QString &name) const;
 
     const QHash<QString, Class>* getClasses() const;
     void setClasses(const QHash<QString, Class> &newClasses);
+    const Class getClassByName(const QString &name) const;
 
     const QHash<QString, Enum>* getEnums() const;
     void setEnums(const QHash<QString, Enum> &newEnums);
+    const Enum getEnumByName(const QString &name) const;
+
+    const Variable getVariableByNameFromCustomData(QString varName, QString dataName) const;
+    const Function getFunctionByNameFromCustomData(QString funcName, QString dataName) const;
+
+    const CustomTypeWithFields getCustomTypeByName(const QString &typeName) const;
 
 private:
     QString expression; // Текстовое выражение
