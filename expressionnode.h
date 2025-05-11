eb3196cec4e38e8c301d196fdf7ca90ea4b9d06b
Paladic <nak78nik@gmail.com>
2025-05-11 18:35:48 +0300

    Установка параметров класса нод

--- expressionnode.h
+++ expressionnode.h
@@ -5,6 +5,16 @@ class ExpressionNode
 {
 public:
     ExpressionNode();
+private:
+    QString value; // содержание ноды
+    ExpressionNode* right;
+    ExpressionNode* left;
+    EntityType nodeType;
+
+    OperationType operType;
+    QString dataType; // тип данных
+    QList<ExpressionNode*>* FunctionArgs; // аргументы функции
+
 };
 
 #endif // EXPRESSIONNODE_H
