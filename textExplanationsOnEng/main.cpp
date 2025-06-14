5a5c5ef47c5e9232ae1791666f3c71b88893c664
Paladic <nak78nik@gmail.com>
2025-06-14 23:23:09 +0300

    Замена тестовых функций на полноценную реализацию

--- textExplanationsOnEng/main.cpp
+++ textExplanationsOnEng/main.cpp
@@ -19,8 +19,6 @@ void printExplanation(QTextStream& cout, const QString& inputFile, const QString
 
 int main(int argc, char *argv[])
 {
-    qDebug() << "Текущая рабочая директория:" << QDir::currentPath();
-
     SetConsoleOutputCP(CP_UTF8);
     QTextStream out(stdout);
     out.setEncoding(QStringConverter::Utf8);
@@ -42,15 +40,9 @@ int main(int argc, char *argv[])
         // Напечатать справочную информацию
         printHelpMessage(cout, fileName);
     }
-    // Если первый аргумент "-test"
-    else if(QString(argv[1]) == "-test") {
-        // Выполнить тесты
-        printTests(cout);
-    }
     // Если аргумента три и второй не начинается с "-"
     else if(argc == 3 && !QString(argv[2]).startsWith("-")) {
-        // Получить объяснение выражения
-		printExplanation(cout, argv[1], argv[2]);
+        printExplanation(cout, argv[1], argv[2]);
     }
     else {
         cout << ("Ошибка в синтаксисе команды. Подробнее: .\\" + fileName +  " -help");
@@ -60,34 +52,45 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void printTests(QTextStream& cout){
-
-    cout << "test\n";
-    return;
 }
 
-void printExplanation(QTextStream& cout, const QString& inputFile, const QString& outputFile){
+// Функция для записи текста в файл
+void writeToFile(const QString& filePath, const QString& content) {
+    QFile file(filePath);
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QTextStream out(&file);
+        out << content;
+        file.close();
+    } else {
+        throw TEException(ErrorType::OutputFileCannotBeCreated, QList<QString>{filePath});
+    }
+}
 
+void printExplanation(QTextStream& cout, const QString& inputFile, const QString& outputFile) {
     try {
+        // Проверить доступ к выходному файлу
+        checkFileAccess(outputFile);
         // Считать входной файл
-        // Expression expr = Expression(inputFile);
-        // // Получить объяснение выражения
-        // cout << expr.getExplanationInEn();
-        //cout << "\nfinal: success";
-    }
-    catch(TEException exception) {
-        cout << exception.what();
-
-        cout << "\nfinal: error";
+        Expression exp = Expression::fromFile(inputFile);
+        // Получить объяснение выражения
+        QString explanation = exp.getExplanationInEn();
+        // Вывести объяснение в консоль
+        cout << explanation;
+        // Записать объяснение в выходной файл
+        writeToFile(outputFile, explanation);
+    } catch (QList<TEException>& errors) {
+        for (const TEException& error : errors) {
+            cout << error.what() << "\n";
+        }
+    } catch (TEException& error) {
+        cout << error.what();
     }
 }
-
 
 void printHelpMessage(QTextStream& cout, const QString& filename)
 {
     cout << ".\\" + filename + " [-help | -test] [input-file] [output-file]\n";
     cout << "-help      - Выводит сообщение-помощник. При вводе этой команды путь к файлам указывать не нужно.\n";
-    cout << "-test      - Запускает тесты. При вводе этой команды путь к файлам указывать не нужно.\n";
     cout << "input-file - путь к входному файлу. В случае, если в пути файла присутствуют пробелы, необходимо указать путь в кавычках. Например:\n";
     cout << "               \"C:\\\\input files\\input.txt\"\n";
     cout << "output-file - путь к выходному файлу. Если файла не существует - он будет создан. В случае, если в пути файла присутствуют пробелы, необходимо указать путь в кавычках. Например:\n";
