c929f7b079c872bb61c804b47c85d42d76246523
Paladic <nak78nik@gmail.com>
2025-06-15 18:17:48 +0300

    Изменение шаблонов

--- textExplanationsOnEng/expressiontranslator.cpp
+++ textExplanationsOnEng/expressiontranslator.cpp
@@ -1,7 +1,9 @@
 #include "expressiontranslator.h"
+#include "teexception.h"
 
 const QHash<OperationType, QString> ExpressionTranslator::Templates = {
     {OperationType::Addition, "sum of {1} and {2}"},
+    {OperationType::Concatenation, "concatenation of {1} and {2}"},
     {OperationType::Subtraction, "difference of {1} and {2}"},
     {OperationType::And, "{1} and {2}"},
     {OperationType::Or, "{1} or {2}"},
@@ -17,21 +19,30 @@ const QHash<OperationType, QString> ExpressionTranslator::Templates = {
     {OperationType::Not, "not {1}"},
     {OperationType::UnaryMinus, "negation of {1}"},
     {OperationType::AddressOf, "get the address of the element {1}"},
-    {OperationType::Dereference, "{1}"},
+    {OperationType::Dereference, "accessing the value at {1}"},
     {OperationType::ArrayAccess, "element at index {2} of array {1}"},
-    {OperationType::FieldAccess, "{1}’s {2}"},
-    {OperationType::PointerFieldAccess, "{1}’s {2}"},
-    {OperationType::StaticMemberAccess, "{1}’s {2}"},
-    {OperationType::PrefixIncrement, "increment {1}, then get…"},
-    {OperationType::PostfixIncrement, "get…, then increment {1}"},
-    {OperationType::PrefixDecrement, "decrement {1}, then get…"},
-    {OperationType::PostfixDecrement, "get…, then decrement {1}"},
+    {OperationType::FieldAccess, "{1}'s {2}"},
+    {OperationType::PointerFieldAccess, "{1}'s {2}"},
+    {OperationType::StaticMemberAccess, "{2}"},
+    {OperationType::PrefixIncrement, "increment {1}, then get {2}"},
+    {OperationType::PostfixIncrement, "get {2}, then increment {1}"},
+    {OperationType::PrefixDecrement, "decrement {1}, then get {2}"},
+    {OperationType::PostfixDecrement, "get {2}, then decrement {1}"},
     {OperationType::Assignment, "assign {2} to {1}"},
     {OperationType::AdditionAssignment, "add {2} to {1} and assign the result to {1}"},
     {OperationType::SubtractionAssignment, "subtract {2} from {1} and assign the result to {1}"},
     {OperationType::MultiplicationAssignment, "multiply {1} by {2} and assign the result to {1}"},
     {OperationType::DivisionAssignment, "divide {1} by {2} and assign the result to {1}"},
     {OperationType::ModulusAssignment, "assign the remainder of {1} divided by {2} to {1}"},
+    {OperationType::NotLessThan, "{1} is not less than {2}"},
+    {OperationType::NotLessThanOrEqual, "{1} is not less than or equal to {2}"},
+    {OperationType::NotGreaterThan, "{1} is not greater than {2}"},
+    {OperationType::NotGreaterThanOrEqual, "{1} is not greater than or equal to {2}"},
+    {OperationType::PointerIndexAccess, "get the element at the index equal to the pointer of {1}"},
+    {OperationType::SubtractionSequence, "difference of {1} and the sum of {2}"},
+    {OperationType::DivisionSequence, "quotient of {1} and the product of {2}"},
+    {OperationType::SingleDecrement, "decrement {1}"},
+    {OperationType::SingleIncrement, "increment {1}"},
     };
 
 ExpressionTranslator::ExpressionTranslator()
