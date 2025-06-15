26cac20f8842b55c4cf901dec6c09eab3cc63131
Paladic <nak78nik@gmail.com>
2025-06-15 18:18:59 +0300

    Добавление новых типов операций

--- textExplanationsOnEng/codeentity.h
+++ textExplanationsOnEng/codeentity.h
@@ -3,6 +3,7 @@
 
 #include <QList>
 #include <QHash>
+#include <QSet>
 #include <QString>
 
 enum class EntityType {
@@ -36,6 +37,7 @@ enum class OperationType {
     Division,                 // Деление
     Modulus,                  // Остаток от деления
     Addition,                 // Сложение
+    Concatenation,            // Конкатенация
     Subtraction,              // Вычитание
     LessThan,                 // Оператор сравнения «меньше»
     GreaterThan,              // Оператор сравнения «больше»
@@ -52,6 +54,16 @@ enum class OperationType {
     PostfixDecrement,         // Постфиксный декремент
     PostfixIncrement,         // Постфиксный инкремент
     StaticMemberAccess,       // Обращение к статическому элементу
+    NotLessThan,              // Логическое "не меньше"
+    NotLessThanOrEqual,       // Логическое "не меньше или равно"
+    NotGreaterThan,           // Логическое "не больше"
+    NotGreaterThanOrEqual,    // Логическое "не больше или равно"
+    PointerIndexAccess,       // Получение элементу по индексу указателя
+    SubtractionSequence,      // Операции вычитания подряд
+    DivisionSequence,         // Операции деления подряд
+    SingleIncrement,          // Один инкремент
+    SingleDecrement,          // Один декремент
+    FunctionCall,
     None
 };
 
