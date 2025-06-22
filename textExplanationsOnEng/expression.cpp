386fe4ad3a4e7affec4f94d0dff6139fc44fcb54
Paladic <nak78nik@gmail.com>
2025-06-22 21:00:41 +0300

    Реализация функции разреза выражения

--- textExplanationsOnEng/expression.cpp
+++ textExplanationsOnEng/expression.cpp
@@ -321,6 +321,50 @@ QString Expression::getExplanationInEn()
     return explanation;
 }
 
+QStringList Expression::splitExpression(const QString &str) {
+    QStringList tokens;
+    QString currentToken;
+    bool insideQuotes = false;
+
+    for (int i = 0; i < str.size(); ++i) {
+        QChar currentChar = str[i];
+        // Если обнаружили кавычку, переключаем режим обработки
+        if (currentChar == '"') {
+            insideQuotes = !insideQuotes;
+            currentToken += currentChar;
+        }
+        // Если находимся внутри кавычек, добавляем символ в текущий токен
+        else if (insideQuotes) {
+            currentToken += currentChar;
+        }
+        // Если пробел и не внутри кавычек, завершаем текущий токен
+        else if (currentChar.isSpace()) {
+            if (!currentToken.isEmpty()) {
+                tokens.append(currentToken);
+                currentToken.clear();
+            }
+        }
+        // Иначе добавляем символ в текущий токен
+        else {
+            currentToken += currentChar;
+        }
+    }
+    // Добавляем последний токен, если он не пустой
+    if (!currentToken.isEmpty()) {
+        tokens.append(currentToken);
+    }
+    return tokens;
+}
+
+QString Expression::sanitizeDataType(const QString& dataType) {
+    if (dataType.contains('[')) {
+        return dataType.left(dataType.indexOf('['));
+    } else if (dataType.contains('*')) {
+        return dataType.left(dataType.indexOf('*'));
+    }
+    return dataType;
+}
+
 ExpressionNode* Expression::expressionToNodes() {
     // Разделяем выражение на лексемы
     QStringList tokens = expression.split(' ', Qt::SkipEmptyParts);
